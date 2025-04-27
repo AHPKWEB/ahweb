2708a77a38239bc214c79284f6d30457217fb21f
ABU HASHIM <152430830+AHPKWEB@users.noreply.github.com>
2025-04-27 23:50:03 +0400

    Update Add.js

--- src/HW/Add.js
+++ src/HW/Add.js
@@ -1,25 +1,4 @@
 
-// Android and Windows
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-
-  setTimeout(() => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted A2HS prompt');
-        } else {
-          console.log('User dismissed A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-    }
-  }, 5000); // Show after 5 seconds
-});
-
 // iPhone Safari
 window.addEventListener('load', function() {
   const isIos = /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
